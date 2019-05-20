0c0e3bbdcde0e549a932620d0c6760580a7fa176
freyamade <frybrdrck@gmail.com>
2019-05-20 22:15:09 +0100

    Page class copied from ruby lib

---
+++ src/page.cr
@@ -0,0 +1,18 @@
+module XIVAPI
+  # A single page of results from XIVAPI
+  class Page
+    # @return the results
+    getter :results
+
+    # @return [Integer] the page number of the next results
+    getter :next_page
+
+    # Creates an object for storing a Page of results from XIVAPI
+    # @param response The response of an HTTP request
+    def initialize(response)
+      @results = response.results
+      pagination = response.pagination
+      @next_page = pagination.page_next unless pagination.page == pagination.page_total
+    end
+  end
+end
