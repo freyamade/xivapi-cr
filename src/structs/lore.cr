2eb3e9f686033c5ff26a962b3e0d0cd394aa8bfc
freya <fb@cix.ie>
2019-05-28 00:53:16 +0100

    Moved LoreData struct into lore.cr

--- src/structs/lore.cr
+++ src/structs/lore.cr
@@ -2,6 +2,44 @@ require "json"
 
 module XIVAPI
   module Structs
+    # Struct for the Data column of the /lore/ endpoint data
+    struct LoreData
+      # define a JSON mapping to create instances of this struct
+      JSON.mapping(
+        id: {type: Int32, key: "ID"},
+        icon: {type: String, key: "Icon"},
+        name: {type: String, key: "Name"},
+        name_cn: {type: String?, key: "Name_cn"},
+        name_de: {type: String?, key: "Name_de"},
+        name_en: {type: String?, key: "Name_en"},
+        name_fr: {type: String?, key: "Name_fr"},
+        name_ja: {type: String?, key: "Name_ja"},
+        name_kr: {type: String?, key: "Name_kr"},
+        url: {type: String, key: "Url"},
+      )
+
+      # id of the item represented in the lore
+      getter id
+      # icon for the piece of lore
+      getter icon
+      # name in the api language
+      getter name
+      # name of the item in chinese
+      getter name_cn
+      # name of the item in german
+      getter name_de
+      # name of the item in english
+      getter name_en
+      # name of the item in french
+      getter name_fr
+      # name of the item in japanese
+      getter name_ja
+      # name of the item in korean
+      getter name_kr
+      # url of the page of the item in the lore
+      getter url
+    end
+
     # Struct for items returned from the /lore/ endpoint
     struct Lore
       # define a JSON mapping to create instances of this struct
