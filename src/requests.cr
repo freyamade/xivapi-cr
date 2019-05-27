876e6860b6f13371bb3e134bc462fc215a095a66
freya <fb@cix.ie>
2019-05-27 22:25:36 +0100

    Added requests.cr file with a map of endpoints to return types
    This file helps me keep track of structs I still need to write

---
+++ src/requests.cr
@@ -0,0 +1,38 @@
+require "./structs/*"
+
+module XIVAPI
+  ENDPOINT_MAP = {
+    "/search"                                         => Array(Structs::SearchResult),
+    "/lore"                                           => Array(Structs::Lore),
+    "/content"                                        => Array(String),
+    "/servers"                                        => Array(String),
+    "/servers/dc"                                     => HashMap(String, Array(String)),
+    "/market/{server}/item/{item_id}"                 => HashMap(String, String),
+    "/market/item/{item_id}?servers=[]"               => HashMap(String, String),
+    "/market/item?servers=[]&items=[]"                => HashMap(String, String),
+    "/market/ids"                                     => Array(Int32),
+    "/market/categories"                              => Array(String),                  # Some kind of ItemSearchCategory struct? Might be also used elsewhere
+    "/character/search?name=First+Last&server=Server" => Array(HashMap(String, String)), # Character data
+    "/character/{id}"                                 => HashMap(String, String),
+    "/character/{id}/verification"                    => HashMap(String, String),
+    "/character/{id}/update"                          => Int32,
+    "/freecompany/search"                             => Array(HashMap(String, String)),
+    "/freecompany/{id}"                               => HashMap(String, String),
+    "/linkshell/search"                               => Array(HashMap(String, String)),
+    "/linkshell/{id}"                                 => HashMap(String, String),
+    "/pvpteam/search"                                 => Array(HashMap(String, String)),
+    "/pvpteam/{id}"                                   => HashMap(String, String),
+    "/lodestone/news"                                 => Arrah(HashMap(String, String)),
+    "/lodestone/notices"                              => Arrah(HashMap(String, String)),
+    "/lodestone/maintenance"                          => Arrah(HashMap(String, String)),
+    "/lodestone/updates"                              => Arrah(HashMap(String, String)),
+    "/lodestone/status"                               => Arrah(HashMap(String, String)),
+    "/lodestone/worldstatus"                          => Arrah(HashMap(String, String)),
+    "/lodestone/devblog"                              => Arrah(Structs::Devblog),
+    "/lodestone/devposts"                             => Arrah(Structs::Devpost),
+    "/lodestone/deepdungeon"                          => Arrah(Structs::DeepDungeon),
+    "/lodestone/heavenonhigh"                         => Arrah(Structs::DeepDungeon),
+    "/lodestone/feasts"                               => Arrah(Structs::Feast),
+    "/patchlist"                                      => Array(Structs::Patch),
+  }
+end
