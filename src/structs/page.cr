3d8353caaeccd1700864696d9cc61c27e91c418f
freya <fb@cix.ie>
2019-06-05 18:21:45 +0100

    Added a page struct for page based requests

---
+++ src/structs/page.cr
@@ -0,0 +1,17 @@
+require "./pagination"
+
+module XIVAPI
+  # A struct that represents a page of results from paginated endpoints.
+  # The Page struct is a generic, so that we can use it for all kinds of endpoints.
+  struct Page(T)
+    JSON.mapping(
+      results : Array(T),
+      pagination : Struct::Pagination
+    )
+
+    # The results contained in the page
+    getter results
+    # An `XIVAPI::Structs::Pagination` struct containing the pagination info for the request
+    getter pagination
+  end
+end
