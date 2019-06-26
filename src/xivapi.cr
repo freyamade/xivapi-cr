c071d2ebdf5e0f7b03764a598baedbdd9626bce5
freya <fb@cix.ie>
2019-06-26 17:39:07 +0100

    Moved client to its own file, in order to start a client folder
    Each file in the client folder will extend the class with the
    necessary methods for each group of endpoints

--- src/xivapi.cr
+++ src/xivapi.cr
@@ -1,45 +1,8 @@
-require "./structs/*"
+require "./client/client"
 
 module XIVAPI
   VERSION = "0.1.0"
 
   # All of the possible language options
   LANGUAGE_OPTIONS = %w(en ja de fr cn kr).freeze
-
-  # Top level API client to interact with the API
-  class Client
-    @api_key : String
-    @staging : Bool
-    @language : String = "en"
-    @poll_rate : UInt32
-
-    def initialize(@api_key : String = "", language : String = "en", @poll_rate : UInt32 = 5, @staging : Bool = false)
-      # Use the setter method for language to ensure it's in the array of allowed languages
-      self.language = language
-    end
-
-    # Set the language of the client by ensuring it's in the array of allowed languages
-    def language=(language : String)
-      language = language.downcase
-      raise ArgumentError, "Unsupported language: #{language}. Allowed choices are #{LANGUAGE_OPTIONS}." unless LANGUAGE_OPTIONS.includes? language
-      @language = language
-    end
-
-    # Get a hash map of default parameters for requests
-    def default_params
-      return {
-        private_key: @api_key,
-        language:    @language,
-      }
-    end
-
-    # The api key for the client, can be changed while in use
-    property api_key
-    # A flag stating whether or not requests should use the stage version of the API
-    property staging
-    # The current language being used as the default for the requests
-    getter language
-    # The rate at which to poll cached requests
-    property poll_rate
-  end
 end
