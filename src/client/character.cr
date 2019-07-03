b1e4c187ea448209061c4083fab1e7ba3559306a
freyamade <frybrdrck@gmail.com>
2019-07-03 12:50:48 +0100

    Initial character client file written with character searching

---
+++ src/client/character.cr
@@ -0,0 +1,32 @@
+require "./client"
+
+module XIVAPI
+  class Client
+    # In this file, add methods for polling Character data
+
+    # Subset of ENDPOINT MAP in requests.cr pertaining to Character requests
+    ENDPOINT_MAP = {
+      "/character/search?name=First+Last&server=Server" => Structs::CharacterSearch, # Character data
+      "/character/{id}"                                 => Structs::Character,
+      "/character/{id}/verification"                    => Structs::Verification,
+      "/character/{id}/update"                          => Int32,
+    }
+
+    # Search the lodestone API for characters with a given name.
+    # Optionally, search specific servers and
+    def character_search(name : String, server : String = "", page : UInt32 = 0) : Structs::Page(Structs::CharacterSearch)
+      # Send a request to the API for character data, sending the passed parameters along with it
+      params = {
+        "name"   => name,
+        "server" => server,
+        "page"   => page.to_s,
+      }
+      response = request "/character/search", params
+      return Structs::Page(Structs::CharacterSearch).from_json response
+    end
+  end
+end
+
+client = XIVAPI::Client.new staging: true
+page = client.character_search "Erika Vaincannet"
+puts page.results[0].id
