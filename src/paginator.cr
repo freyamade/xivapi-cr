be90352f8fd452fad32650a89bd07b0fc94b43e4
freyamade <frybrdrck@gmail.com>
2019-07-03 12:00:18 +0100

    Fixing up paginator class

--- src/paginator.cr
+++ src/paginator.cr
@@ -3,12 +3,21 @@ require "./structs/page"
 module XIVAPI
   # A class that abstracts iterating through pages of API data by implementing `Enumerable`
   class Paginator(T)
-    include Enumerable
+    # include Enumerable
+    JSON.mapping(
+      pagination: {type: Structs::Pagination, key: "Pagination"},
+      results: {type: Array(T), key: "Results"},
+    )
 
     # TODO - Fill in instance variables
 
-    def each : Structs::Page(T)
-      return Page(T).from_json "{}"
-    end
+    # def each : Structs::Page(T)
+    #   return Page(T).from_json "{}"
+    # end
+
+    # Pagination data
+    getter pagination
+    # Results Array
+    getter results
   end
 end
