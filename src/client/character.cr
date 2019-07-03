801d1e815d9e9a4f43d9a50afd073e3b3d07d597
freyamade <frybrdrck@gmail.com>
2019-07-03 13:40:02 +0100

    Added character read method to client

--- src/client/character.cr
+++ src/client/character.cr
@@ -6,15 +6,15 @@ module XIVAPI
 
     # Subset of ENDPOINT MAP in requests.cr pertaining to Character requests
     ENDPOINT_MAP = {
-      "/character/search?name=First+Last&server=Server" => Structs::CharacterSearch, # Character data
+      "/character/search?name=First+Last&server=Server" => Structs::CharacterSummary, # Character data
       "/character/{id}"                                 => Structs::Character,
-      "/character/{id}/verification"                    => Structs::Verification,
+      "/character/{id}/verification"                    => Structs::CharacterVerification,
       "/character/{id}/update"                          => Int32,
     }
 
     # Search the lodestone API for characters with a given name.
-    # Optionally, search specific servers and
-    def character_search(name : String, server : String = "", page : UInt32 = 1) : Structs::Page(Structs::CharacterSearch)
+    # Optionally, search specific servers.
+    def character_search(name : String, server : String = "", page : UInt32 = 1) : Structs::Page(Structs::CharacterSummary)
       # Send a request to the API for character data, sending the passed parameters along with it
       params = {
         "name"   => name,
@@ -22,7 +22,15 @@ module XIVAPI
         "page"   => page.to_s,
       }
       response = request "/character/search", params
-      return Structs::Page(Structs::CharacterSearch).from_json response
+      return Structs::Page(Structs::CharacterSummary).from_json response
+    end
+
+    # Retrieve the details of the Character with the given Lodestone ID.
+    # For now, sends a flag to retrieve extended character data from the API.
+    def character_read(id : UInt64) : Structs::Character
+      endpoint = "/character/#{id}?extended=1"
+      response = request endpoint
+      return Structs::Character.from_json response
     end
   end
 end
