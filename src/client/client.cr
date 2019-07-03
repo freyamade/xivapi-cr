f699a75e53e35803b71674a2144898fbe867561a
freyamade <frybrdrck@gmail.com>
2019-07-03 12:50:28 +0100

    More client preparation

--- src/client/client.cr
+++ src/client/client.cr
@@ -1,35 +1,59 @@
 require "http/client"
+require "http/params"
+require "../structs/*"
 
 module XIVAPI
   # Top level API client to interact with the API
+  API_URL = "https://xivapi.com"
+  # Top level API client to interact with the staging API
+  STAGE_URL = "https://staging.xivapi.com"
+  # The allowed language options
+  LANGUAGE_OPTIONS = %w(en ja de fr cn kr)
+
   class Client
     @api_key : String
-    @staging : Bool
+    @staging : Bool = false
     @language : String = "en"
     @poll_rate : UInt32
+    @url : String = API_URL
 
     def initialize(@api_key : String = "", language : String = "en", @poll_rate : UInt32 = 5, @staging : Bool = false)
       # Use the setter method for language to ensure it's in the array of allowed languages
+      self.staging = staging
       self.language = language
+    end
+
+    # Set the staging value of the client.
+    # This method automatically updates the API url to the appropriate value
+    def staging=(staging : Bool)
+      @staging = staging
+      @url = staging ? STAGE_URL : API_URL
     end
 
     # Set the language of the client by ensuring it's in the array of allowed languages
     def language=(language : String)
       language = language.downcase
-      raise ArgumentError, "Unsupported language: #{language}. Allowed choices are #{LANGUAGE_OPTIONS}." unless LANGUAGE_OPTIONS.includes? language
+      raise ArgumentError.new("Unsupported language: #{language}. Allowed choices are #{LANGUAGE_OPTIONS}.") unless LANGUAGE_OPTIONS.includes? language
       @language = language
     end
 
-    # Make a request to a specific endpoint and return the HTTP body of the response, to be parsed in other methods
+    # Make a request to a specific endpoint and return the HTTP body of the response, to be parsed in other methods.
+    # Attaches the supplied endpoint to the the current in-use url value
     # TODO - Overload to take in extra parameters
     private def request(endpoint : String) : String
-      return HTTP::Client.get(endpoint).body
+      url = "#{@url}#{endpoint}"
+      return HTTP::Client.get(url).body
+    end
+
+    private def request(endpoint : String, params : Hash(String, String)) : String
+      endpoint = "#{endpoint}?#{HTTP::Params.encode params}"
+      return request endpoint
     end
 
     # The api key for the client, can be changed while in use
     property api_key
     # A flag stating whether or not requests should use the stage version of the API
-    property staging
+    getter staging
     # The current language being used as the default for the requests
     getter language
     # The rate at which to poll cached requests
