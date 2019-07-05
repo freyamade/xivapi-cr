e41a56b05b054a41a6fff1c05228645913a11d1f
freya <fb@cix.ie>
2019-07-05 13:26:10 +0100

    Removed test.cr as it's now in the client_specs

--- test.cr
+++
@@ -1,8 +0,0 @@
-require "./src/client/*"
-
-client = XIVAPI::Client.new
-page = client.character_search "Erika Vaincannet"
-id = page.results[0].id
-puts client.character(id).info.not_nil!.character.not_nil!.updated
-puts client.character_verification(id, "-").bio
-puts client.character_update id
