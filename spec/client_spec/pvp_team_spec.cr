41c855ffb4f58f31935a59c070311afcbd87ad33
freyamade <frybrdrck@gmail.com>
2019-07-08 00:05:48 +0100

    Added initial specs for the pvp team methods

---
+++ spec/client_spec/pvp_team_spec.cr
@@ -0,0 +1,33 @@
+require "../spec_helper"
+
+describe XIVAPI::Client do
+  # Series of tests for the PvpTeam Client methods
+  describe "PvpTeam" do
+    it "successfully searches for PvpTeam" do
+      client = XIVAPI::Client.new
+      page = client.pvp_team_search "a"
+      # We don't usually get results from this, so as long as no exception is raised, we're grand.
+    end
+
+    it "successfully raises an exception if an invalid search is sent" do
+      client = XIVAPI::Client.new
+      ex = expect_raises(XIVAPI::Exceptions::XIVAPIException) do
+        client.pvp_team_search ""
+      end
+      ex.api_exception.message.should eq "You must provide a name to search."
+    end
+
+    # I'd love to be able to do this but the search method returns no results so I guess we can't right now.
+    # it "reads a PvpTeam properly" do
+    #   client = XIVAPI::Client.new
+    #   # Get the ID by doing a search and grabbing the first one.
+    #   page = client.pvp_team_search "a"
+    #   pvp_team = page.results[0]
+    #   pvp_team_response = client.pvp_team pvp_team.id
+
+    #   # Ensure we get a meaningful response
+    #   pvp_team_response.pvp_team.should_not be_nil
+    #   pvp_team_response.info.pvp_team.should_not be_nil
+    # end
+  end
+end
