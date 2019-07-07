e2c360e3f55fabf498c96caf98c57c501f84e04c
freyamade <frybrdrck@gmail.com>
2019-07-08 00:05:33 +0100

    Added pvp team methods to the Client

--- src/client/pvp_team.cr
+++ src/client/pvp_team.cr
@@ -1,4 +1,36 @@
-# ENDPOINT_MAP = {
-#   "/pvpteam/search" => Array(HashMap(String, String)),
-#   "/pvpteam/{id}"   => HashMap(String, String),
-# }
+require "./client"
+
+module XIVAPI
+  class Client
+    # In this file, add methods for retrieving PvpTeam data
+
+    # Search the Lodestone API for PvP Teams with a given name.
+    # Optionally, search specific servers.
+    # NOTE: This method isn't fully test, since any requests we made to the endpoint returned no results.
+    def pvp_team_search(name : String, server : String = "", page : UInt32 = 1) : Dataclasses::Page(Dataclasses::PvpTeamProfile)
+      # Send a request to the API for pvp team data, sending the passed parameters along with it
+      params = {
+        "name"   => name,
+        "server" => server,
+        "page"   => page.to_s,
+      }
+      response = request "pvpteam/search", params
+      begin
+        return Dataclasses::Page(Dataclasses::PvpTeamProfile).from_json response
+      rescue
+        raise Exceptions::XIVAPIException.new(Dataclasses::Exception.from_json response)
+      end
+    end
+
+    # Retrieve the details of the PvpTeam with the given Lodestone ID.
+    def pvp_team(id : String) : Dataclasses::PvpTeamResponse
+      endpoint = "pvpteam/#{id}"
+      response = request endpoint, {"data" => "PVP"}
+      begin
+        return Dataclasses::PvpTeamResponse.from_json response
+      rescue
+        raise Exceptions::XIVAPIException.new(Dataclasses::Exception.from_json response)
+      end
+    end
+  end
+end
