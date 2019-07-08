6bb12891d949ae3191932448b40bdedc6d0a1452
freya <fb@cix.ie>
2019-07-08 17:29:36 +0100

    Typo

--- src/client/pvp_team.cr
+++ src/client/pvp_team.cr
@@ -6,7 +6,7 @@ module XIVAPI
 
     # Search the Lodestone API for PvP Teams with a given name.
     # Optionally, search specific servers.
-    # NOTE: This method isn't fully test, since any requests we made to the endpoint returned no results.
+    # NOTE: This method isn't fully tested, since any requests we made to the endpoint returned no results.
     def pvp_team_search(name : String, server : String = "", page : UInt32 = 1) : Dataclasses::Page(Dataclasses::PvpTeamProfile)
       # Send a request to the API for pvp team data, sending the passed parameters along with it
       params = {
