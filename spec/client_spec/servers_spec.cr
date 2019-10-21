28c12a0876e9e20fa5a7cbf51f8664deb0e10161
freya <fb@cix.ie>
2019-10-21 15:54:48 +0100

    Changing spec names to make output look nicer

--- spec/client_spec/servers_spec.cr
+++ spec/client_spec/servers_spec.cr
@@ -1,16 +1,14 @@
 require "../spec_helper"
 
-describe XIVAPI::Client do
-  # Series of tests for the Servers Client methods
-  describe "Servers" do
-    it "retrieves an Array of Servers" do
-      client = XIVAPI::Client.new
-      servers = client.servers
-    end
+# Series of tests for the Servers Client methods
+describe "XIVAPI::Client::Servers" do
+  it "retrieves an Array of Servers" do
+    client = XIVAPI::Client.new
+    servers = client.servers
+  end
 
-    it "retrieves a Hash of Datacenter information" do
-      client = XIVAPI::Client.new
-      datacenters = client.datacenters
-    end
+  it "retrieves a Hash of Datacenter information" do
+    client = XIVAPI::Client.new
+    datacenters = client.datacenters
   end
 end
