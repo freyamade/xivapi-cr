6322c8cde48d5474ce7beebb65288ab74cb5bd7a
freya <fb@cix.ie>
2019-10-21 15:47:43 +0100

    Removed lodestone methods because they were removed from the API

--- src/dataclasses/dataclasses.cr
+++ src/dataclasses/dataclasses.cr
@@ -1,8 +1,6 @@
 require "./character/*"
 require "./free_company/*"
 require "./linkshell/*"
-require "./lodestone/*"
-require "./market/*"
 require "./other/*"
 require "./pvp_team/*"
 require "./utils/*"
