7b148e6fc29c04cfefff8991eb527dab73090ac3
freya <fb@cix.ie>
2019-10-21 14:22:09 +0100

    Fixing other issues after removing the market file

--- spec/regression_spec.cr
+++ spec/regression_spec.cr
@@ -2,11 +2,6 @@ require "./spec_helper"
 
 # Special test suite for found bugs to make sure we don't regress
 describe "Regression Tests" do
-  it "can parse untradeable items" do
-    client = XIVAPI::Client.new
-    client.market(item_id: 20770_u64, server: "Lich")
-  end
-
   it "correctly searches for 'Iron Claw Hammer'" do
     client = XIVAPI::Client.new
     client.search(string: "Iron Claw Hammer", indexes: ["Item"], string_algo: "match")
